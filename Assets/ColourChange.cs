6ff0fb65527aa502292045cc1c28776fe0a54d03
TheLStar <ElliottS595@msn.com>
2015-02-09 21:53:53 +0000

    BoxingFun
    BoxActivity

---
+++ Assets/ColourChange.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColourChange : MonoBehaviour {
+
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+						gameObject.renderer.material.color = Color.red;
+				}
+	
+	}
+}
